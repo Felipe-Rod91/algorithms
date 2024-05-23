ccfc4e0d61bc4009dbb61b7408686a67d2be83e2
Felipe Rodrigues <felipe.git@outlook.com>
2024-05-23 16:02:28 -0300

    Algoritmos gulosos, todos os Estados e em quais deles as rádios tocam

---
+++ 08-algoritmos_gulosos.py
@@ -0,0 +1,9 @@
+# Todos os Estados em que precisamos espalhar as rádios
+states_needed = set(['mt', 'wa', 'or', 'id', 'nv', 'ut', 'ca', 'az'])
+# Todas as estações e em quais Estados elas tocam
+stations = {}
+stations['kone'] = set(['id', 'nv', 'ut'])
+stations['ktwo'] = set(['wa', 'id', 'mt'])
+stations['kthree'] = set(['or', 'nv', 'ca'])
+stations['kfour'] = set(['nv', 'ut'])
+stations['kfive'] = set(['ca', 'az'])
